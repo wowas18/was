14a0ca92e13e0662b841681cc21e1c7231031a02
wowas18 <wowas18@nm.ru>
2018-07-12 13:04:53 +0400

    x

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 set -e
 
 # set the postgres database host, port, user and password according to the environment
